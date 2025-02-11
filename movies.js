cd19fb2c69b6b463d9c29290afeda28a58b39d41
Igor Gabriel <81106970+IPlisetsky@users.noreply.github.com>
2025-02-11 09:39:13 -0300

    Update movies.js

--- movies.js
+++ movies.js
@@ -1,28 +1,35 @@
-
-//document.getElementById("gerar").addEventListener("click", function(event) {
-//    let generoSelected = document.getElementById("generos").value;
-//    let url = "http://127.0.0.1:5000/gerar";
-//    if (generoSelected) {
-//        url += `?genero=${generoSelected}`;
-//    }
-//
-//    fetch(url)
-
-
 document.getElementById("gerar").addEventListener("click", function() {
     let generoSelected = document.getElementById("generos").value;
+    let anoValue = document.getElementById("ano").value; // Get the value from the input 'ano'
+    let anoInicio = document.getElementById("ano-inicio").value;
+    let anoFim = document.getElementById("ano-fim").value;
+
+
+
     let url = "http://127.0.0.1:5000/gerar";
     
     if (generoSelected) {
         url += `?generos=${generoSelected}`;
     }
     
-    fetch(url)
-
     
+    if (anoInicio) {
+        url += `&ano-inicio=${anoInicio}`;
+    }
+    
+    if (anoFim) {
+        url += `&ano-fim=${anoFim}`;
+    }
+    
+
+    if (anoValue) {
+        url += `&ano=${anoValue}`;
+    }
+    
+    fetch(url)
     .then(response => {
         if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`); // Trata erros HTTP
+            throw new Error(`HTTP error! status: ${response.status}`);
         }
         return response.json();
     })
@@ -32,11 +39,10 @@ document.getElementById("gerar").addEventListener("click", function() {
             return;
         }
 
-        document.getElementById("titulo").innerText = data.titulo;
-        document.getElementById("genero").innerText = data.genero.join(', '); // Join the list into a string for display
+        document.getElementById("titulo").innerText = `${data.titulo} (${data.ano_lancamento})`;
+        document.getElementById("genero").innerText = data.genero.join(', ');
         document.getElementById("link").innerHTML = `<a href="${data.link}" target="_blank">Mais informação</a>`;
         document.getElementById("image_poster").src = "https://image.tmdb.org/t/p/w500" + data.poster;     
-        
     })
     .catch(error => {
         console.error("Erro:", error);
